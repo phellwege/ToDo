ec0f47cf87eb026910d4c8b38d406872cb143fb8
phellweg <peter.hellwege@vizientinc.com>
2022-06-13 13:33:07 -0600

    added svg elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import './Header.css'
+import {ReactComponent as Sun} from '../static/icon-sun.svg';
+import {ReactComponent as Moon} from '../static/icon-moon.svg';
 
 export default () => {
 
     return (
         <div className='HeadWrap'>
-            <div>Left</div>
-            <div>Right</div>
+            <h1>Left</h1>
+            <div>
+                <Sun className='svg' id='sun'/>
+                <Moon className='svg' id='moon'/>
+            </div>
         </div>
     )
 }
