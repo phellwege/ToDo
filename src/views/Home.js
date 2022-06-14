513013de9282e888c899ee75015118d051604e90
phellweg <peter.hellwege@vizientinc.com>
2022-06-14 13:38:21 -0600

    working on basic show/hide logic

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,26 @@
 // import React from 'react';
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import {ReactComponent as CheckBox} from '../static/icon-check.svg';
 import {ReactComponent as Cross} from '../static/icon-cross.svg';
 
-export default () => {
 
+export default () => {
+    function showSvg() {
+        document.getElementById('CheckSvg').style.visibility = 'visible';
+        document.getElementById('CrossSvg').style.visibility = 'visible';
+    }
+    function hideSvg() {
+        document.getElementById('CheckSvg').style.visibility = 'hidden';
+        document.getElementById('CrossSvg').style.visibility = 'hidden';
+    }
+    function checkTheBox() {
+        document.getElementById('CheckSvg').style.visibility = 'visible';
+    }
+const [listItem, setListItem] = useState('') 
+const [isShown, setIsShown] = useState(false)
     return (
         <div className="background">
             <Header/>
@@ -16,18 +30,18 @@ export default () => {
                 </div>
                 <div className="HomeBottom">
                     <ul>
-                        <li>
-                            <CheckBox className="svg" id='CheckSvg'/>
+                        <li onMouseEnter={showSvg} onMouseLeave={hideSvg}>
+                            <CheckBox className="svg" id='CheckSvg' onClick={checkTheBox}/>
                             Items in list
                             <Cross className="svg" id='CrossSvg'/>
                         </li>
-                        <li>
-                            <CheckBox className="svg" id='CheckSvg'/>
+                        <li onMouseEnter={showSvg} onMouseLeave={hideSvg}>
+                                <CheckBox className="svg" id='CheckSvg' onClick={checkTheBox}/>
                             Items in list
                             <Cross className="svg" id='CrossSvg'/>
                         </li>
-                        <li>
-                            <CheckBox className="svg" id='CheckSvg'/>
+                        <li onMouseEnter={showSvg} onMouseLeave={hideSvg}>
+                            <CheckBox className="svg" id='CheckSvg' onClick={checkTheBox}/>
                             Items in list
                             <Cross className="svg" id='CrossSvg'/>
                         </li>
